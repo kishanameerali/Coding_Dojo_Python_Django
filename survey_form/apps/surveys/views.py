e5a06f6ceab38b72b15c776045d046b7da1958ee
Kishan <kishan.ameerali@gmail.com>
2017-08-10 00:37:53 -0400

    Fourth commit, adding Survey Form

---
+++ survey_form/apps/surveys/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+def index(request):
+    return render(request, 'surveys/index.html')
+
+def process(request):
+    request.session['name'] = request.POST['name']
+    request.session['location'] = request.POST['location']
+    request.session['language'] = request.POST['language']
+    request.session['comment'] = request.POST['comment']
+    return redirect('/')
+
+# Create your views here.
