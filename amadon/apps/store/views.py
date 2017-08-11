9aadc745f3f4426a8b2c01a29f5d333bfd80927c
Kishan <kishan.ameerali@gmail.com>
2017-08-10 21:34:46 -0400

    Seventh commit, updated assignment Amadon

--- amadon/apps/store/views.py
+++ amadon/apps/store/views.py
@@ -4,29 +4,33 @@ def index(request):
     return render(request, 'store/index.html')
 
 def buy(request):
-    request.session['quantity'] = request.POST['quantity']
-    items_ordered = 0
-    total_purchases = 0
+    request.session['quantity'] = int(request.POST['quantity'])
+    
     if request.POST['product_id'] == 1:
         price = 19.99
-        purchase = price * request.session['quantity']
-        items_ordered += 1
-        total_purchases += purchase
+        request.session['purchase'] = price * request.session['quantity']
     elif request.POST['product_id'] == 2:
         price = 29.99
         purchase = price * request.session['quantity']
-        items_ordered += 1
-        total_purchases += purchase
     elif request.POST['product_id'] == 3:
         price = 4.99
         purchase = price * request.session['quantity']
-        items_ordered += 1
-        total_purchases += purchase
     elif request.POST['product_id'] == 4:
         price = 49.99
         purchase = price * request.session['quantity']
-        items_ordered += 1
-        total_purchases += purchase
+
+    try:
+        request.session['items_ordered']
+    except KeyError:
+        request.session['items_ordered'] = 0
+    
+    try:
+        request.session['total_purchases']
+    except KeyError:
+        request.session['total_purchases'] = 0
+    
+    request.session['items_ordered'] += request.session['quantity']
+    request.session['total_purchases'] += purchase
     return redirect('/checkout')
 
 def checkout(request):
