371415868d0fab5824e54d6279e0084caa3f7174
Kishan <kishan.ameerali@gmail.com>
2017-08-12 07:50:31 -0400

    Tenth commit, updated assignments Session Words and Ninja Gold

--- ninja_gold/apps/game/views.py
+++ ninja_gold/apps/game/views.py
@@ -20,31 +20,38 @@ def process_money(request):
     if request.POST['building'] == 'farm':
         gold = random.randint(10,21)
         request.session['gold_total'] += gold
-        request.session['result'].insert(0,"Earned " + str(gold) + " from the farm! (" + datetime.datetime.now().strftime('%Y/%m/%d %I:%M') + ")")
+        earnings = "Earned " + str(gold) + " from the farm! (" + datetime.datetime.now().strftime('%Y/%m/%d %I:%M') + ")"
+        earnings_tuple = (earnings, 'green')
+        request.session['result'].insert(0,earnings_tuple)
         print gold
         print request.session['gold_total']
     elif request.POST['building'] == 'cave':
         gold = random.randint(5,11)
         request.session['gold_total'] += gold
-        request.session['result'].insert(0,"Earned " + str(gold) + " from the cave! (" + datetime.datetime.now().strftime('%Y/%m/%d %I:%M') + ")")
-        #session['gold_total'] += random.randint(5,11)
+        earnings = "Earned " + str(gold) + " from the cave! (" + datetime.datetime.now().strftime('%Y/%m/%d %I:%M') + ")"
+        earnings_tuple = (earnings, 'green')
+        request.session['result'].insert(0,earnings_tuple)
         print gold
         print request.session['gold_total']
     elif request.POST['building'] == 'house':
         gold = random.randint(2,5)
         request.session['gold_total'] += gold
-        request.session['result'].insert(0,"Earned " + str(gold) + " from the house! (" + datetime.datetime.now().strftime('%Y/%m/%d %I:%M') + ")")
-        #session['gold_total'] += random.randint(2,5)
+        earnings = "Earned " + str(gold) + " from the house! (" + datetime.datetime.now().strftime('%Y/%m/%d %I:%M') + ")"
+        earnings_tuple = (earnings, 'green')
+        request.session['result'].insert(0,earnings_tuple)
         print gold
         print request.session['gold_total']
     elif request.POST['building'] == 'casino':
         gold = random.randint(-50,51)
         request.session['gold_total'] += gold
         if gold > 0:
-            request.session['result'].insert(0,"Entered a casino and won " + str(gold) + "... YAYYY! (" + datetime.datetime.now().strftime('%Y/%m/%d %I:%M') + ")")
+            earnings = "Entered a casino and won " + str(gold) + "... YAYYY! (" + datetime.datetime.now().strftime('%Y/%m/%d %I:%M') + ")"
+            earnings_tuple = (earnings, 'green')
+            request.session['result'].insert(0,earnings_tuple)
         else:
-            request.session['result'].insert(0,"Entered a casino and lost " + str(gold*(-1)) + "... Ouch! (" + datetime.datetime.now().strftime('%Y/%m/%d %I:%M') + ")")
-        #session['gold_total'] += random.randint(-50,51)
+            earnings = "Entered a casino and lost " + str(gold*(-1)) + "... Ouch! (" + datetime.datetime.now().strftime('%Y/%m/%d %I:%M') + ")"
+            earnings_tuple = (earnings, 'red')
+            request.session['result'].insert(0,earnings_tuple)
         print gold
         print request.session['gold_total']
     return redirect('/')
--- session_words/apps/words_session/views.py
+++ session_words/apps/words_session/views.py
@@ -11,16 +11,20 @@ def index(request):
     return render(request, 'words_session/index.html')
 
 def process(request):
-    request.session['word'] = request.POST['word']
-    request.session['color'] = request.POST['color']
+    word = request.POST['word']
+    color = request.POST['color']
     try:
         request.POST['font_size']
         request.session['font_size'] = True
-        request.session['font'] = '2em'
+        font = '2em'
     except:
         request.session['font_size'] = False
-        request.session['font'] = '1em'
-    request.session['word_total'].insert(0, request.session['word'] + ' added on ' + datetime.datetime.now().strftime('%Y/%m/%d %I:%M'))
+        font = '1em'
+    date_added = datetime.datetime.now().strftime('%Y/%m/%d %I:%M')
+    word_tuple = (word, color, font, date_added)
+    print word_tuple[0]
+    request.session['word_total'].insert(0, word_tuple)
+    print request.session['word_total'][0][1]
     return redirect('/')
 
 def reset(request):
