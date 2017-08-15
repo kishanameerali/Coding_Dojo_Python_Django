8796af2cbd1fbeab8612db88276959d8b8ddae6d
Kishan <kishan.ameerali@gmail.com>
2017-08-15 02:36:16 -0400

    Thirteenth commit, adding assignment Books/Authors, Dojo Ninjas updated as models.py used for Books/Authors

--- dojo_ninjas/apps/dojo_ninja/models.py
+++ dojo_ninjas/apps/dojo_ninja/models.py
@@ -19,5 +19,23 @@ class Student(models.Model):
     def __str__(self):
         return 'First Name: {} Last Name: {}'.format(self.first_name, self.last_name)
 
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
 
-# Create your models here.
+    def __str__(self):
+        return 'Name: {}, Desc: {}'.format(self.name, self.desc)
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    notes = models.TextField
+    books = models.ManyToManyField(Book)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return 'First Name: {}, Last Name {}, Email: {}, Notes: {}'.format(self.first_name, self.last_name, self.email, self.notes)
